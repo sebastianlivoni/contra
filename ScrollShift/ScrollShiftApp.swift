22b42778799cf1f579bfdff63cbce2a40b63c37a
Sebastian <seb@sl1.dk>
2026-06-25 10:19:11 +0200

    Initial Commit

---
+++ ScrollShift/ScrollShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollShiftApp.swift
+//  ScrollShift
+//
+//  Created by Sebastian on 25/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScrollShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
